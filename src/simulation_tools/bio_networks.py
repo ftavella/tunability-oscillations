dc2c5e3233ede135c7b43c87ff1f2e24fbff4314
ftavella <ftavella@umich.edu>
2022-02-16 14:57:31 -0500

    Included methods for the creation of equations inside Network class | Updated tests

--- src/simulation_tools/bio_networks.py
+++ src/simulation_tools/bio_networks.py
@@ -4,103 +4,134 @@
   by the module
 """
 
+from typing import Callable
 import numpy as np
-
-
-def create_gamma_terms(connectivity: np.ndarray) -> list:
-    """Create interaction terms based on the connectivity of the network
-
-    ...
-
-    Parameters
-    ----------
-    connectivity : ndarray
-        Two-dimensional array specifying the interaction among nodes. Only
-        0, 1, and -1 are allowed as interaction types
-
-    Raises
-    ------
-    TypeError
-        If connectivity is not an array
-
-    ValueError
-        If connectivity doesn't have the right dimension or
-        values at each entry
-
-    Returns
-    -------
-    list
-        A list of strings containing the string that encodes for the
-        interaction terms of each node. The dimension matches the number of
-        rows of connectivity
-    """
-
-    if not isinstance(connectivity, np.ndarray):
-        raise TypeError("Input should be a numpy array")
-    if len(connectivity.shape) != 2:
-        raise ValueError("Connectivity matrix should have dimension 2")
-    elif connectivity.shape[0] != connectivity.shape[1]:
-        raise ValueError("Dimensions of connectivity matrix should have \
-                          the same length")
-    elif np.any(~np.isin(np.unique(connectivity), [-1, 0, 1])):
-        raise ValueError("Connectivity matrix should contain only 0, 1, -1")
-    # Write terms
-    terms = ["" for _ in range(connectivity.shape[0])]
-    for idx, row in enumerate(connectivity):
-        terms_in_row = ""
-        for jdx, value in enumerate(row):
-            sign = ""
-            subs = ""
-            if value == 0:
-                continue
-            else:
-                enz = f"x[{jdx}]"
-                pair = f"[{idx},{jdx}]"
-                if value == 1:
-                    sign = "+"
-                    subs = f"(1.0 - x[{idx}])"
-                elif value == -1:
-                    sign = "-"
-                    subs = f"x[{idx}]"
-            # Write term and add it to the terms for this node
-            new_term = f"{sign} G{pair}*{subs}*{enz}**N{pair}/"
-            new_term += f"({enz}**N{pair} + K{pair}**N{pair}) "
-            terms_in_row += new_term
-        terms[idx] = terms_in_row
-    return terms
 
 
 class Network():
     """A class used to represent a biochemical network with defined parameters
 
-    ...
-
     Attributes
     ----------
-    nodes : int
-        Number of nodes in the network.
-    edges : int
-        Number of interactions in the network.
-    period : float
-        If parameters are oscillatory stores the period.
-        If not oscillatory, gets a value of NaN.
-    amplitude : ndarray
-        If parameters are oscillatory stores the amplitude of each node.
-        If not oscillatory, each element of the gets a value of NaN.
+    connectivity : ndarray
+        Two-dimensional array specifying the interaction among nodes. Only
+        0, 1, and -1 are allowed as interaction types
+    equations : function
+        Function containing ODE equations that is suitable for simulation with
+        scipy's solve_ivp module. The equations are defined
 
     Methods
     -------
-    says(sound=None)
-        Prints the animals name and what sound it makesSummary
+    __create_gamma_terms()
+        Creates strings representing the interaction terms of the network
+    __create_function_str()
+        Creates a string defining a python function. The function represents
+        an ODE model the network.
+    __create_equations():
+        Defines a python function encoding the ODE equations of the model.
     """
 
-    def __init__(self, connectivity):
+    def __init__(self, connectivity: np.ndarray):
         """
         Parameters
         ----------
         connectivity : ndarray
-            Numpy array specifying positive or negative interactions between
-            nodes. Self-loops are specified don its diagonal.
+            Two-dimensional array specifying the interaction among nodes. Only
+            0, 1, and -1 are allowed as interaction types
+
+        Raises
+        ------
+        TypeError
+            If connectivity is not a numpy ndarray
+
+        ValueError
+            If connectivity doesn't have the right dimension or
+            values at each entry
         """
 
+        if not isinstance(connectivity, np.ndarray):
+            raise TypeError("Input should be a numpy array")
+        if len(connectivity.shape) != 2:
+            raise ValueError("Connectivity matrix should have dimension 2")
+        elif connectivity.shape[0] != connectivity.shape[1]:
+            raise ValueError("Dimensions of connectivity matrix should have \
+                            the same length")
+        elif np.any(~np.isin(np.unique(connectivity), [-1, 0, 1])):
+            raise ValueError("Input should only contain 0, 1, -1")
+
         self.connectivity = connectivity
+        self.equations = self.__create_equations()
+
+    def __create_gamma_terms(self) -> list:
+        """Creates strings representing the interaction terms of the network
+        based on its connectivity.
+
+        Returns
+        -------
+        list
+            A list of strings containing the string that encodes for the
+            interaction terms of each node. The dimension matches the number of
+            rows of connectivity
+        """
+
+        # Write terms
+        terms = ["" for _ in range(self.connectivity.shape[0])]
+        for idx, row in enumerate(self.connectivity):
+            terms_in_row = ""
+            for jdx, value in enumerate(row):
+                sign = ""
+                subs = ""
+                if value == 0:
+                    continue
+                else:
+                    enz = f"x[{jdx}]"
+                    pair = f"[{idx},{jdx}]"
+                    if value == 1:
+                        sign = "+"
+                        subs = f"(1.0 - x[{idx}])"
+                    elif value == -1:
+                        sign = "-"
+                        subs = f"x[{idx}]"
+                # Write term and add it to the terms for this node
+                new_term = f"{sign} G{pair}*{subs}*{enz}**N{pair}/"
+                new_term += f"({enz}**N{pair} + K{pair}**N{pair}) "
+                terms_in_row += new_term
+            terms[idx] = terms_in_row
+        return terms
+
+    def __create_function_str(self) -> str:
+        """Creates a string defining a python function with an ODE model
+        following the connectivity of the network.
+
+        Returns
+        -------
+        str
+            A string defining the ODE equations as a python function.
+        """
+
+        G_terms = self.__create_gamma_terms()
+        fn_template = "def equations(t, x, A, B, G, K, N):"
+        return_template = "\n\treturn np.array(["
+        for idx, terms in enumerate(G_terms):
+            fn_template += f"\n\tdx{idx}dt ="
+            fn_template += f" B[{idx}]*(A[{idx}] - x[{idx}]) {terms}"
+            return_template += f"dx{idx}dt,"
+        fn_template += return_template[:-1] + "])"
+        return fn_template
+
+    def __create_equations(self) -> Callable:
+        """Defines a python function encoding the ODE equations of the model
+        based on the connectivity of the network.
+
+        Returns
+        -------
+        function
+            A function with the signature: equations(t, x, A, B, G, K, N).
+            Connectivity defines the terms included in each equation.
+        """
+
+        fn_str = self.__create_function_str()
+        loc = {}
+        exec(fn_str, globals(), loc)
+        equations = loc['equations']
+        return equations
--- test/test_bio_networks.py
+++ test/test_bio_networks.py
@@ -6,6 +6,7 @@ import os
 import sys
 import pytest
 import numpy as np
+from scipy.integrate import solve_ivp
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '../src'))
 sys.path.append(ROOT_DIR)
 from simulation_tools import bio_networks as bn  # noqa : E402
@@ -13,36 +14,68 @@ from simulation_tools import bio_networks as bn  # noqa : E402
 
 class TestBioNetworks():
 
-    def test_goodwin_gamma_terms(self):
-        connectivity = np.array([[0, 0, 1], [1, 0, 0], [0, -1, 0]])
-        term_0 = "+ G[0,2]*(1.0 - x[0])*x[2]**N[0,2]/"
-        term_0 += "(x[2]**N[0,2] + K[0,2]**N[0,2]) "
-        term_1 = "+ G[1,0]*(1.0 - x[1])*x[0]**N[1,0]/"
-        term_1 += "(x[0]**N[1,0] + K[1,0]**N[1,0]) "
-        term_2 = "- G[2,1]*x[2]*x[1]**N[2,1]/"
-        term_2 += "(x[1]**N[2,1] + K[2,1]**N[2,1]) "
-        terms = [term_0, term_1, term_2]
-        assert terms == bn.create_gamma_terms(connectivity)
-
-    def test_input_empty_array(self):
+    def test_empty_connectivity_as_input(self):
+        connectivity = np.array([])
         with pytest.raises(ValueError):
-            bn.create_gamma_terms(np.array([]))
+            _ = bn.Network(connectivity)
 
-    def test_input_not_array(self):
+    def test_str_connectivity_as_input(self):
+        connectivity = ""
         with pytest.raises(TypeError):
-            bn.create_gamma_terms("")
+            _ = bn.Network(connectivity)
 
-    def test_input_incorrect_values(self):
+    def test_incorrect_values_in_connectivty(self):
         connectivity = np.array([[0, 0, 2], [1, 0, 0], [0, -1, 0]])
         with pytest.raises(ValueError):
-            bn.create_gamma_terms(connectivity)
+            _ = bn.Network(connectivity)
 
-    def test_input_incorrect_dimension(self):
+    def test_incorrect_dimension_in_connectivity(self):
         connectivity = np.array([[0, 0, 1], [1, 0, 0], [0, -1, 0], [1, 0, 0]])
         with pytest.raises(ValueError):
-            bn.create_gamma_terms(connectivity)
+            _ = bn.Network(connectivity)
 
-    def test_network_initialization(self):
+    def test_network_has_connectivity(self):
         connectivity = np.array([[0, 0, 1], [1, 0, 0], [0, -1, 0]])
-        net = bn.Network(connectivity)
-        assert np.all(net.connectivity == connectivity)
+        network = bn.Network(connectivity)
+        assert np.all(network.connectivity == connectivity)
+
+    def test_network_creates_appropriate_equations(self):
+        connectivity = np.array([[0, 0, 1], [1, 0, 0], [0, -1, 0]])
+        network = bn.Network(connectivity)
+        # Oscillating parameters
+        A = np.array([0.00004564, 0.16342066, 0.70351496])
+        B = np.array([56.67356362, 50.87885942, 30.31904319])
+        G = np.array([[0, 0, 1657.39796806],
+                     [6988.78292809, 0, 0],
+                     [0, 7461.31128709, 0]])
+        K = np.array([[0, 0, 0.11196140],
+                     [0.50912113, 0, 0],
+                     [0, 0.33210760, 0]])
+        N = np.array([[0, 0, 3.67709117],
+                     [7.17307667, 0, 0],
+                     [0, 6.80130072, 0]])
+
+        def expected_equations(t, x, A, B, G, K, N):
+            dx0dt = B[0]*(A[0] - x[0])
+            dx1dt = B[1]*(A[1] - x[1])
+            dx2dt = B[2]*(A[2] - x[2])
+            # Interactions
+            dx0dt += + G[0, 2] * \
+                (1.0 - x[0]) * x[2] ** N[0, 2] / \
+                (x[2] ** N[0, 2] + K[0, 2] ** N[0, 2])
+            dx1dt += + G[1, 0] * \
+                (1.0 - x[1]) * x[0] ** N[1, 0] / \
+                (x[0] ** N[1, 0] + K[1, 0] ** N[1, 0])
+            dx2dt += - G[2, 1] * \
+                x[2] * x[1] ** N[2, 1] / \
+                (x[1] ** N[2, 1] + K[2, 1] ** N[2, 1])
+            return np.array([dx0dt, dx1dt, dx2dt])
+
+        expected_solution = solve_ivp(expected_equations, [0, 1], [0, 0, 0],
+                                      method="LSODA", args=([A, B, G, K, N]))
+        network_solution = solve_ivp(network.equations, [0, 1], [0, 0, 0],
+                                     method="LSODA", args=([A, B, G, K, N]))
+
+        assert np.all(expected_solution.y[0, :] == network_solution.y[0, :])
+        assert np.all(expected_solution.y[1, :] == network_solution.y[1, :])
+        assert np.all(expected_solution.y[2, :] == network_solution.y[2, :])
